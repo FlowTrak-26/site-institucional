51bf12b654e770fe95ed632c8ef6cf4f5d0b60de
vitorluzsantos <vitor.lsantos@sptech.school>
2026-06-05 11:14:45 -0300

    feat: nav-bar muda se voce logar

--- web-data-viz/public/sessao.js
+++ web-data-viz/public/sessao.js
@@ -1,27 +1,29 @@
 // sessão
 function validarSessao() {
+
 	var email = sessionStorage.EMAIL_USUARIO;
 	var nome = sessionStorage.NOME_USUARIO;
 	var permissao = sessionStorage.PERMISSAO; // validar se é admin se for ele pode ter acesso a pg perfil empresa
 
-user_name.innerHTML = sessionStorage.NOME_USUARIO;
-user_permissao.innerHTML = sessionStorage.PERMISSAO;
+	// user_name.innerHTML = sessionStorage.NOME_USUARIO;
+	// user_permissao.innerHTML = sessionStorage.PERMISSAO;
+
 
 	// var b_usuario = document.getElementById("b_usuario");
 	var user_name = document.getElementById('user_name');
 	var user_permissao = document.getElementById('user_permissao');
 
-	if (email != null && nome != null) {
-		user_name.innerHTML = nome;
-		user_permissao = permissao;
-	} else {
-		window.location = "../login.html";
-	}
+	// if (email != null && nome != null) {
+	// 	user_name.innerHTML = nome;
+	// 	user_permissao.innerHTML = permissao;
+	// } else {
+	// 	window.location = "../login.html";
+	// }
 }
 
 function limparSessao() {
 	sessionStorage.clear();
-	window.location = "../login.html";
+	window.location = "./main/login.html";
 }
 
 // carregamento (loading)
