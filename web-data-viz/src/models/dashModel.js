60f1a37484dcfe5fb20793e01697088801866b05
KarinaCupolaAlves <karina.alves@sptech.school>
2026-06-06 23:53:13 -0300

    Alterando gráficos

--- web-data-viz/src/models/dashModel.js
+++ web-data-viz/src/models/dashModel.js
@@ -18,17 +18,31 @@ function buscarDadosGraficoLinha(idEmpresa, limite_linhas) {
             ON dc.fk_sensor = sn.id_sensor
             WHERE emp.id_empresa = ${idEmpresa}
             ORDER BY dc.data_hora DESC
-            LIMIT 7;
+            LIMIT ${limite_linhas};
     `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function atualizarDadosGraficoLinha(id_grafico) {
+function atualizarDadosGraficoLinha(idEmpresa) {
 
      var instrucaoSql = 
-     ``;
+     `SELECT
+            emp.id_empresa,
+            pt.nome AS nome_ponto,
+            sn.id_sensor AS nome_sensor,
+            data_hora, dc.fluxo
+            FROM empresa_parceira emp
+            JOIN ponto_monitoramento pt
+            ON pt.fk_empresa = emp.id_empresa
+            JOIN sensor sn
+            ON sn.fk_ponto = pt.id_ponto
+            JOIN dado_captado dc
+            ON dc.fk_sensor = sn.id_sensor
+            WHERE emp.id_empresa = ${idEmpresa}
+            ORDER BY dc.data_hora DESC
+            LIMIT 1;`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
