53f3f8de9bd03e602c99d80e84afdf3d171c07df
victor-sousao <victor.soliveira@sptech.school>
2026-06-08 09:27:21 -0300

    Adicionando funcao de buscar por cpf

--- web-data-viz/src/models/usuarioModel.js
+++ web-data-viz/src/models/usuarioModel.js
@@ -9,6 +9,10 @@ function autenticar(email, senha) {
     return database.executar(instrucaoSql);
 }
 
+function buscarPorCpf(cpf){
+    var instrucaoSql =`SELECT * FROM usuario WHERE cpf = ${cpf}`
+}
+
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
 function cadastrar(nome, email, senha, fk_empresa_parceira) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fk_empresa_parceira);
