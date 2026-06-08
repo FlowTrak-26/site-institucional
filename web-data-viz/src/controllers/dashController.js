70a4f2d6c3407d1c7da6ecdcdb29da796de8cc23
EmanuellyMel <emanuelly.melo@sptech.school>
2026-06-07 23:21:56 -0300

    slteracoes do grafico de linha da dash esp

--- web-data-viz/src/controllers/dashController.js
+++ web-data-viz/src/controllers/dashController.js
@@ -83,10 +83,100 @@ function atualizarDadosMapaCalor(req, res) {
     });
 }
 
+function buscarDadosGraficoLinhaEsp(req, res) {
+
+    var idEmpresa = req.params.idEmpresa;
+    var idpontoMonitoramento = req.params.idpontoMonitoramento;
+
+
+    console.log(`Recuperando medidas em tempo real`);
+
+    dashModel.atualizarDadosMapaCalor(idpontoMonitoramento, idEmpresa).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+
+     
+}
+
+function atualizarDadosGraficoLinhaEsp(req, res) {
+
+    var idEmpresa = req.params.idEmpresa;
+    var idpontoMonitoramento = req.params.idpontoMonitoramento;
+
+    console.log(`Recuperando medidas em tempo real`);
+
+    dashModel.atualizarDadosGraficoLinha(idpontoMonitoramento, idEmpresa).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function buscarDadosMapaCalorEsp(req, res) {
+
+    const limite_linhas = 7;
+
+    var idEmpresa = req.params.idEmpresa;
+    var idpontoMonitoramento = req.params.idpontoMonitoramento;
+
+
+    console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
+
+    dashModel.buscarDadosMapaCalor(idpontoMonitoramento, idEmpresa, limite_linhas).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
+function atualizarDadosMapaCalorEsp(req, res) {
+
+    var idpontoMonitoramento = req.params.idpontoMonitoramento;
+    var idEmpresa = req.params.idEmpresa;
+
+    console.log(`Recuperando medidas em tempo real`);
+
+    dashModel.atualizarDadosMapaCalor(idpontoMonitoramento, idEmpresa).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
     module.exports = {
     buscarDadosGraficoLinha,
     atualizarDadosGraficoLinha,
     buscarDadosMapaCalor,
-    atualizarDadosMapaCalor
+    atualizarDadosMapaCalor,
+    buscarDadosGraficoLinhaEsp,
+    atualizarDadosGraficoLinhaEsp,
+    buscarDadosMapaCalorEsp,
+    atualizarDadosMapaCalorEsp
 }
 
--- web-data-viz/src/routes/dash.js
+++ web-data-viz/src/routes/dash.js
@@ -21,5 +21,25 @@ router.get("/tempo-real/grafico-calor/:idEmpresa", function (req, res) {
     dashController.atualizarDadosMapaCalor(req, res);
 })
 
+
+
+router.get("/ultimas/grafico-linha/:idEmpresa/:idpontoMonitoramento", function (req, res) {
+    dashController.buscarDadosGraficoLinhaEsp(req, res);
+});
+
+router.get("/tempo-real/grafico-linha/:idEmpresa/:idpontoMonitoramento", function (req, res) {
+    dashController.atualizarDadosGrafico(req, res);
+})
+
+
+
+router.get("/ultimas/grafico-calor/:idEmpresa/:idpontoMonitoramento", function (req, res) {
+    dashController.buscarDadosMapaCalorEsp(req, res);
+});
+
+router.get("/tempo-real/grafico-calor/:idEmpresa/:idpontoMonitoramento", function (req, res) {
+    dashController.atualizarDadosMapaCalorEsp(req, res);
+})
+
 module.exports = router;
 
